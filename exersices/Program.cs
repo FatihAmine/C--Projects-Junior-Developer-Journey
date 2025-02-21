2f6f5bec4ee6fad87952348b9ea30533aa6a5f6f
FatihAmine <aminefatih333@gmail.com>
2025-02-21 20:21:19 +0100

    Add project files.

---
+++ exersices/Program.cs
@@ -0,0 +1,146 @@
+﻿
+/*
+
+// minor or adult
+
+Console.WriteLine("Enter Your Age PLS: ");
+
+int age = Convert.ToInt32(Console.ReadLine());
+
+if (age < 18)
+{
+    Console.WriteLine("You are  a minor ");
+}
+else
+{
+    Console.WriteLine("You are an adult ");
+}
+
+
+// Even or ODD
+
+Console.WriteLine("Enter Your number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("The number is Even ");
+}
+else
+{
+    Console.WriteLine("The number is Odd ");
+}
+
+
+// Temperature converter
+
+Console.WriteLine(" Welcome to Temperature converter ");
+
+Console.WriteLine("First You need to enter the  temperature in Celsius and converts it to Fahrenheit ");
+
+double celsius = Convert.ToDouble(Console.ReadLine());
+
+double fahrenheit ;
+
+fahrenheit = (celsius * 9 / 5) + 32; 
+
+Console.WriteLine("temperature in fahrenheit is: " + fahrenheit);
+
+
+// Check Voting Eligibility
+
+Console.WriteLine("Check Voting Eligibility");
+
+int age = Convert.ToInt32(Console.ReadLine());
+
+if (age >= 18)
+{
+    Console.WriteLine("You can vote");
+}
+else
+{
+    Console.WriteLine("You cannot vote");
+}
+ 
+
+// Number Sign Checker
+
+Console.WriteLine("Number Sign Checker");
+
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number > 0)
+{
+    Console.WriteLine("positive");
+}
+else if (number < 0)
+{
+    Console.WriteLine("negative");
+}
+else
+{
+    Console.WriteLine("zero");
+}
+
+
+// Grade Calculator
+
+Console.WriteLine("Grade Calculator");
+Console.WriteLine("Enter grade percentage");
+int grade = Convert.ToInt32(Console.ReadLine());
+
+if (grade >= 0 && grade <= 59)
+{
+    Console.WriteLine("F");
+}
+else if (grade >= 60 && grade <= 69 )
+{
+    Console.WriteLine("D");
+}
+else if (grade >= 70 && grade <= 79)
+{
+    Console.WriteLine("C");
+}
+else if (grade >= 80 && grade <= 89)
+{
+    Console.WriteLine("B");
+}
+else if (grade >= 90 && grade <= 100)
+{
+    Console.WriteLine("A");
+}
+else
+{
+    Console.WriteLine(" Enter Number bettwen 0-100 ");
+}
+
+
+// Max of Two Numbers
+
+Console.WriteLine("Max of Two Numbers");
+
+Console.WriteLine("Enter your First number ");
+int nb1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter your Second number ");
+int nb2 = Convert.ToInt32(Console.ReadLine());
+
+if ( nb1 > nb2)
+{
+
+    Console.WriteLine("The First Number is greater ");
+
+}
+else if ( nb1 < nb2)
+{
+
+    Console.WriteLine("The Second Number is greater ");
+
+}
+else
+{
+    Console.WriteLine("The Numbers are equal ");
+}
+
+
+*/
